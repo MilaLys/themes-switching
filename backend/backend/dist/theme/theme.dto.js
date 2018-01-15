195a3467e7f3e1ab277059a173f1c55c1084618c
Liudmila Lysenko <llysenko23@rambler.ru>
2018-01-15 17:31:17 +0200

    feat(cd): settings for cd on heroku

--- backend/backend/dist/theme/theme.dto.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateThemeDto {
-}
-exports.CreateThemeDto = CreateThemeDto;
-//# sourceMappingURL=theme.dto.js.map
--- backend/backend/dist/theme/theme.schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-exports.ThemeSchema = new mongoose.Schema({
-    name: String
-});
-//# sourceMappingURL=theme.schema.js.map
